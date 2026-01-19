e7d167ce7e29774e53eeb4c1a681b0741d4fea5f
wind <wzsf1810@qq.com>
2026-01-19 17:07:05 +0800

    补充部分文档

--- help/docs/.vitepress/config.ts
+++ help/docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitepress'
 
-const base = './help/docs/.vitepress/dist'
+const base = '/help/docs/.vitepress/dist'
 
 export default defineConfig({
   title: 'MyObj 帮助文档',
